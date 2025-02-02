03370bb4a6e9db42ed9e1c1c7d2c574e7aef0b59
lu-dwig <murimiludwig@gmail.com>
2025-02-03 01:16:05 +0300

    tsting of the threads api

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,14 @@ const ReplyModel = require("../models").Reply;
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board').post((req, res) => {
+    const{ text, delete_password } = req.body;
+      let board =req.body.board;
+      if (!board){
+        board = req.params.board;
+      }
+    res.json({ test: "test"});
+  });
     
   app.route('/api/replies/:board');
 
