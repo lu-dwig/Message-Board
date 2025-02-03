2868603291a1b80bfac97f458469310c208266ef
lu-dwig <murimiludwig@gmail.com>
2025-02-04 00:10:49 +0300

    database function properties changes

--- db-connection.js
+++ db-connection.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
-const db =mongoose.connect(process.env.DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
-module.exports = db;
+// Connect to MongoDB
+// const uri = 'mongodb+srv://Schleswig:zHD7Drhxu3rAi48R@cluster0.u51m4.mongodb.net/messageboard?retryWrites=true&w=majority&appName=Cluster0'
+//   mongoose.connect(uri, { 
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true 
+//   });
+// const db =mongoose.connect(process.env.DB, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+const uri =mongoose.connect( uri, process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+module.exports = uri;
 
 // require('dotenv').config({ path: '.env' });
