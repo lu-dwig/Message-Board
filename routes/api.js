8451d032ba832480553a095850a109a0e1c3c052
lu-dwig <murimiludwig@gmail.com>
2025-02-07 21:24:39 +0300

    addition of the thread req

--- routes/api.js
+++ routes/api.js
@@ -78,8 +78,10 @@ module.exports = function (app) {
               reported,
               delete_password,
               replies,
-            }
-          })
+              replycount: thread.replies.length,
+            };
+          });
+          res.json(threads);
         }
       })
   });  
